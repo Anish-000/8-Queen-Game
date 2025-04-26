065cfcc82fe0b59cf8dabb9e91188f05b4dcb814
Anish Chattopadhyay <165559685+Anish-000@users.noreply.github.com>
2025-04-27 05:13:18 +0530

    GlassQueen.js

--- src/components/GlassQueen.js
+++ src/components/GlassQueen.js
@@ -4,26 +4,28 @@ import '../styles/GlassQueen.css';
 const GlassQueen = () => {
   return (
     <div className="glass-queen">
-      <div className="cross"></div>
       <div className="crown">
+        <div className="crown-top"></div>
         <div className="crown-points">
           <span></span>
           <span></span>
           <span></span>
           <span></span>
           <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
         </div>
+        <div className="crown-ring"></div>
       </div>
-      <div className="neck"></div>
+      <div className="neck">
+        <div className="neck-detail"></div>
+      </div>
       <div className="body">
+        <div className="body-highlight"></div>
         <div className="body-detail"></div>
         <div className="body-glow"></div>
       </div>
       <div className="base">
         <div className="base-ring"></div>
+        <div className="base-glow"></div>
       </div>
     </div>
   );
